548b7d2948ef1b27790e1d21dc05182ec6656d04
suhaimitoamy <suhaimitoamy@gmail.com>
2026-06-21 23:46:05 +0800

    Convert HTML app to Android project for APK build

---
+++ app/src/main/java/com/amyelitesuite/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.amyelitesuite
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        val webView = WebView(this)
+        setContentView(webView)
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
