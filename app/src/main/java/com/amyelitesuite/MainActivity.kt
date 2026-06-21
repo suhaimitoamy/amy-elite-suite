8045982108c8fadb58edcf0d221847a8d3d2f39e
suhaimitoamy <34860608+suhaimitoamy@users.noreply.github.com>
2026-06-22 07:05:28 +0800

    Enable WebView back navigation

--- app/src/main/java/com/amyelitesuite/MainActivity.kt
+++ app/src/main/java/com/amyelitesuite/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.amyelitesuite
 
+import android.app.Activity
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.app.Activity
 
 class MainActivity : Activity() {
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        val webView = WebView(this)
+
+        webView = WebView(this)
         setContentView(webView)
 
         val webSettings: WebSettings = webView.settings
         webSettings.javaScriptEnabled = true
         webSettings.domStorageEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
 
         webView.webViewClient = WebViewClient()
         webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
