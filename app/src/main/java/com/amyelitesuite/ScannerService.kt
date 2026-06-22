cbe28f5db2aef6557aadcd5fa0fa1e7b372276ef
suhaimitoamy <suhaimitoamy@gmail.com>
2026-06-22 20:11:16 +0800

    fix: resolve Android 14 Foreground Service crash by adding dataSync type and permission

--- app/src/main/java/com/amyelitesuite/ScannerService.kt
+++ app/src/main/java/com/amyelitesuite/ScannerService.kt
@@ -48,7 +48,11 @@ class ScannerService : Service() {
                 .build()
         }
 
-        startForeground(1, notification)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(1, notification, android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+        } else {
+            startForeground(1, notification)
+        }
         return START_STICKY
     }
 
